2e0a420ad6db8d2adac95ee9c510bae371523ef0
chasedurrett <chase.durrett80@gmail.com>
2020-05-28 12:28:11 -0500

    init commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default props => (
-    <div className='header-container'>
-        <h1>React Routing</h1>
-        <nav>
-            <span>Home</span>
-            <span>About</span>
-            <span>Contact</span>
-        </nav>
-    </div>
-)
+export default (props) => (
+  <div className="header-container">
+    <h1>React Routing</h1>
+    <nav>
+      <Link className="nav-links" to="/">
+        Home
+      </Link>
+      <Link className="nav-links" to="/about">
+        About
+      </Link>
+      <Link className="nav-links" to="/contact">
+        Contact
+      </Link>
+    </nav>
+  </div>
+);
